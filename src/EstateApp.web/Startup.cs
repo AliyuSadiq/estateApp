2310e20f4f8db43b7019e63b9de140f9e4d110eb
AliyuSadiq <57262107+AliyuSadiq@users.noreply.github.com>
2020-05-29 09:36:09 +0100

    added database to our web app

--- src/EstateApp.web/Startup.cs
+++ src/EstateApp.web/Startup.cs
@@ -18,11 +18,18 @@ namespace EstateApp
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) 
+        {
+            this.Configuration = configuration;
+               
+        }
+                public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AuthenticationDbContext>(options => options.UseSqlServer
+            (Configuration.GetConnectionString("AuthenticationConnection")));
             services.AddControllersWithViews();
         }
 
